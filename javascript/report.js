5113c6b24f7752a064fdd5d816c81dd054c1aabe
Peiyu Wang <eraince@hotmail.com>
2017-12-11 21:32:49 -0600

    add chart and basic styling.

--- javascript/report.js
+++ javascript/report.js
@@ -1,30 +1,70 @@
+var svg = d3.select("svg"),
+    margin = {top: 10, right: 10, bottom: 10, left: 40},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom;
+  
+var tooltip = d3.select("body").append("div").attr("class", "toolTip");
 
+var x = d3.scaleBand().rangeRound([0, width]).padding(0.35),
+    y = d3.scaleLinear().rangeRound([height, 0]);
+  
+var colours = d3.scaleOrdinal()
+    .range(["#24C2CB", "#E1B753","#DA2536","#789F8A"]);
 
-var data = [4, 8, 15, 16, 23, 42];
+var g = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var width = 420,
-    barHeight = 20;
+d3.json("report.json", function(error, data) {
+    if (error) throw error;
 
-var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, width]);
+    x.domain(data.map(function(d) { return d.pieces; }));
+    y.domain([0, d3.max(data, function(d) { return d.value; })]);
 
-var chart = d3.select(".chart")
-    .attr("width", width)
-    .attr("height", barHeight * data.length);
+    g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")");
+        // .call(d3.axisBottom(x));
 
-var bar = chart.selectAll("g")
-    .data(data)
-  .enter().append("g")
-    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+    g.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y).ticks(5).tickFormat(function(d) { return parseInt(d / 1) + "%"; }).tickSizeInner([-width]))
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .attr("fill", "#5D6971")
+        .text("Average House Price - (£)");
 
-bar.append("rect")
-    .attr("width", x)
-    .attr("height", barHeight - 1);
+    g.selectAll(".bar")
+        .data(data)
+      .enter().append("rect")
+        .attr("x", function(d) { return x(d.pieces); })
+        .attr("y", function(d) { return y(d.value); })
+        .attr("width", x.bandwidth())
+        .attr("height", function(d) { return height - y(d.value); })
+        .attr("fill", function(d) { return colours(d.pieces); })
+        .on("mousemove", function(d){
+            tooltip
+              .style("left", d3.event.pageX - 50 + "px")
+              .style("top", d3.event.pageY - 70 + "px")
+              .style("display", "inline-block")
+              .html((d.pieces) + "<br>" + "£" + (d.value));
+        })
+            .on("mouseout", function(d){ tooltip.style("display", "none");});
 
-bar.append("text")
-    .attr("x", function(d) { return x(d) - 3; })
-    .attr("y", barHeight / 2)
-    .attr("dy", ".35em")
-    .text(function(d) { return d; });
+     g.selectAll(".bar")
+        .data(data)
+      .enter().append("text")
+      .attr("class", "barLabel")
+        .style("text-anchor", "end")
+        .style("fill", "white")
+        .attr("transform", function(d){
+            var xText = x(d.pieces) + x.bandwidth() / 2;
+            var yText = y(d.value);
+            return "translate(" + xText + "," + yText + ") rotate(-90)";
+        })
+        .text(function(d) { return d.pieces;});
+    });
 
+    
