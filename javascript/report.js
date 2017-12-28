635a86e2c9beb729e03f563c5ce10d395baa3ad1
Peiyu Wang <eraince@hotmail.com>
2017-12-28 14:52:39 -0600

    testing.

--- javascript/report.js
+++ javascript/report.js
@@ -45,7 +45,7 @@ d3.json("report.json", function(error, data) {
         .attr("width", x.bandwidth())
         .attr("height", function(d) { return height - y(d.value); })
         .attr("fill", function(d) { return colours(d.pieces); })
-        .on("mousemove", function(d){
+        .on("click", function(d){
             tooltip
               .style("left", d3.event.pageX - 50 + "px")
               .style("top", d3.event.pageY - 70 + "px")
